278a0dc983c8d15624f0c0a26a66af09ad411e78
fabio eduardo <circuncisaofabio@icloud.com>
2023-02-28 19:32:14 -0300

    modificado pelo eslint

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Model } from "mongoose";
+import { Tasks, TasksDocuments } from "src/schemas/tasks.schemas";
+import { InjectModel } from "@nestjs/mongoose";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectModel(Tasks.name)
+    private TaskModel: Model<TasksDocuments>
+  ) {}
+}

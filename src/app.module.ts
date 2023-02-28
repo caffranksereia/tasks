7eb33012d497d90cb185fc4b6e78ca758a615360
fabio eduardo <circuncisaofabio@icloud.com>
2023-02-28 19:32:50 -0300

    incluiu o mongoose forfeature eo pacote do axios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersService } from './users/users.service';
-import { UsersModule } from './users/users.module';
-import { TasksModule } from './tasks/tasks.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { UsersService } from "./users/users.service";
+import { UsersModule } from "./users/users.module";
+import { TasksModule } from "./tasks/tasks.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [AuthModule, UsersModule, TasksModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TasksModule,
+    MongooseModule.forRoot("mongodb://localhost:27017"),
+  ],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { TasksService } from './tasks.service';
-import { TasksController } from './tasks.controller';
+import { Module } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
+import { TasksController } from "./tasks.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Tasks, TaksSchema } from "src/schemas/tasks.schemas";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Tasks.name,
+        schema: TaksSchema,
+      },
+    ]),
+    HttpModule,
+  ],
   providers: [TasksService],
-  controllers: [TasksController]
+  controllers: [TasksController],
 })
 export class TasksModule {}
